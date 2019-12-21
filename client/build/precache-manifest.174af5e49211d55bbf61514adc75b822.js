002aad6c26f4e5bf1a70d59ff1335a7258c198d7
ghousemohamed <ghousemohamed7126@gmail.com>
2019-12-21 14:47:37 +0530

    Heroku deploy

---
+++ client/build/precache-manifest.174af5e49211d55bbf61514adc75b822.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f39be5d8892c045ff771229ceb3b46b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f00a2c11bee63c641f6",
+    "url": "/static/css/main.2af69a38.chunk.css"
+  },
+  {
+    "revision": "8a676a8919a2a4685afc",
+    "url": "/static/js/2.bc0b6175.chunk.js"
+  },
+  {
+    "revision": "9f00a2c11bee63c641f6",
+    "url": "/static/js/main.203e35b5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require('path');
 
 const app = express();
 
@@ -10,15 +11,23 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.json({ msg: "Welcome to the Contacts Keeper API also this" });
-});
+
 
 //Define Routes
 
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
+
+// Serve Static assets in production
+
+if(process.env.NODE_ENV === 'production') {
+  // Set Static folder
+
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 const PORT = process.env.PORT || 5000;
 
